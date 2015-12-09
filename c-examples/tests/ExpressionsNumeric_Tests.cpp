e5259296a59606d034c62e55494987e1845fd4b4
Miran <miranh89@gmail.com>
2015-12-09 14:07:58 +0100

    Added test cases for the minus, sum, abs and difference expressions

--- c-examples/tests/ExpressionsNumeric_Tests.cpp
+++ c-examples/tests/ExpressionsNumeric_Tests.cpp
@@ -8,14 +8,56 @@ extern "C"
 }
 
 
-TEST(Expression_Boolean, minusExp)
+TEST(Expression_Numeric, minusExp)
 {
-//	TVP t = newBool(false);
-//
-//	assert(t->type == VDM_BOOL && "Value is not a bool");
-//	TVP res = vdmNot(t);
-//	EXPECT_EQ (true,res->value.boolVal);
-//	vdmFree(res);
-//
-//	vdmFree(t);
+	TVP t = newInt(5);
+
+	assert(t->type == VDM_INT && "Value is not a integer");
+	TVP res = vdmMinus(t);
+	EXPECT_EQ(-5,res->value.intVal);
+
+	vdmFree(t);
+	vdmFree(res);
+}
+
+TEST(Expression_Numeric, absExp)
+{
+	TVP t = newInt(-5);
+
+	assert(t->type == VDM_INT && "Value is not a integer");
+	TVP res = vdmMinus(t);
+	EXPECT_EQ(5,res->value.intVal);
+
+	vdmFree(t);
+	vdmFree(res);
+}
+
+TEST(Expression_Numeric, sumExp)
+{
+	TVP t1 = newInt(5);
+	TVP t2 = newInt(4);
+
+	assert(t1->type == VDM_INT && "Value is not a integer");
+	assert(t2->type == VDM_INT && "Value is not a integer");
+	TVP res = vdmSum(t1,t2);
+	EXPECT_EQ(9,res->value.doubleVal);
+
+	vdmFree(t1);
+	vdmFree(t2);
+	vdmFree(res);
+}
+
+TEST(Expression_Numeric, differenceExp)
+{
+	TVP t1 = newInt(5);
+	TVP t2 = newInt(4);
+
+	assert(t1->type == VDM_INT && "Value is not a integer");
+	assert(t2->type == VDM_INT && "Value is not a integer");
+	TVP res = vdmDifference(t1,t2);
+	EXPECT_EQ(1,res->value.doubleVal);
+
+	vdmFree(t1);
+	vdmFree(t2);
+	vdmFree(res);
 }
