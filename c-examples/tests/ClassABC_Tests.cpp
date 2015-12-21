33f700efd0503f2a93f7ddb214a40f8e07628d35
Kenneth Lausdahl <lausdahl@eng.au.dk>
2015-12-21 15:53:40 +0100

    renamed test file and cleaned up source code in that test

--- c-examples/tests/ClassABC_Tests.cpp
+++ c-examples/tests/ClassABC_Tests.cpp
@@ -9,35 +9,25 @@ extern "C"
 #include <stdio.h>
 }
 
-
-
-//#define GET_VTABLE_FUNC(tname,ptr,id)   (*( (struct VTable**) (  ((unsigned char*)ptr) + offsetof(struct tname, _##tname##_pVTable) )  ))     [id].pFunc
-#define GET_VTABLE_FUNC(tname,ptr,id)   GET_STRUCT_FIELD(tname,ptr,struct VTable*,_##tname##_pVTable)[id].pFunc
-
-
-
-//*((int*) (((void*) kp) + offsetof(struct KK, c)))
-
-//#define GET_VTABLE_FUNC_CAST(tname,tname2,ptr,id)  ((struct VTable*) *(void**)(ptr+offsetof(struct tname, _##tname##_pVTable )))[id].pFunc
-
+/**
+ * Class handling macros for calling functions and obtaining fields
+ */
 
 //Obtain a struct field
 #define GET_STRUCT_FIELD(tname,ptr,fieldtype,fieldname) (*( (fieldtype*) (  ((unsigned char*)ptr) + offsetof(struct tname, fieldname) )  ))
 
 //Obtain VTable function
-#define GET_VTABLE_FUNC2(thisTypeName,funcTname,ptr,id)   GET_STRUCT_FIELD(thisTypeName,ptr,struct VTable*,_##funcTname##_pVTable)[id].pFunc
+#define GET_VTABLE_FUNC(thisTypeName,funcTname,ptr,id)   GET_STRUCT_FIELD(thisTypeName,ptr,struct VTable*,_##funcTname##_pVTable)[id].pFunc
 
 /*Cast to class pointer by moving it forward to the class specific VTable
  * Note that we only adjust the pointer if a subtype is given (i.e. not the type of it self)
-*/
+ */
 #define CLASS_CAST(ptr,from,to) ((unsigned char*)ptr) + (typeid(from)==typeid(to)?0: offsetof(struct from, _##to##_pVTable))
-//Call function from VTable and change ptr to the correct offset. With arguments
-#define CCCALL(thisTypeName,funcTname,ptr,id,...) GET_VTABLE_FUNC2( thisTypeName,funcTname,ptr,id)(CLASS_CAST(ptr,thisTypeName,funcTname), ## __VA_ARGS__))
-//Call function from VTable and change ptr to the correct offset. With arguments
-//#define CCCALL_VOID(thisTypeName,funcTname,ptr,id) GET_VTABLE_FUNC2( thisTypeName,funcTname,ptr,id)(CLASS_CAST(ptr,thisTypeName,funcTname)))
 
-#define CALL_FUNC(thisTypeName,funcTname,classValue,id, args... )     GET_VTABLE_FUNC2( thisTypeName,funcTname,TO_CLASS_PTR(classValue,thisTypeName),id)(CLASS_CAST(TO_CLASS_PTR(classValue,thisTypeName),thisTypeName,funcTname), ## args)
-//#define CALL_FUNC_ARGS(thisTypeName,funcTname,classValue,id,...) GET_VTABLE_FUNC2( thisTypeName,funcTname,TO_CLASS_PTR(classValue,thisTypeName),id)(CLASS_CAST(TO_CLASS_PTR(classValue,thisTypeName),thisTypeName,funcTname,__VA_ARGS__))
+//Call function from VTable and change ptr to the correct offset. With obtional arguments
+#define CCCALL(thisTypeName,funcTname,ptr,id,...) GET_VTABLE_FUNC( thisTypeName,funcTname,ptr,id)(CLASS_CAST(ptr,thisTypeName,funcTname), ## __VA_ARGS__))
+
+#define CALL_FUNC(thisTypeName,funcTname,classValue,id, args... )     GET_VTABLE_FUNC( thisTypeName,funcTname,TO_CLASS_PTR(classValue,thisTypeName),id)(CLASS_CAST(TO_CLASS_PTR(classValue,thisTypeName),thisTypeName,funcTname), ## args)
 
 #define TO_CLASS_PTR(tv,type) ((struct type *) ( ((struct ClassType*)tv->value.ptr)->value))
 //
@@ -63,34 +53,6 @@ void checkFreeDouble(const char* name, double expected, TVP value)
  * Tests
  */
 
-TEST(A, _newDirectVTable)
-{
-	TVP c=A._new();
-	UNWRAP_CLASS_A(l, c);
-
-	TVP a = newInt(1);
-	TVP b = newInt(4);
-
-	struct VTable* tbl = GET_STRUCT_FIELD(A,l,struct VTable*,_A_pVTable);
-
-	TVP res = GET_VTABLE_FUNC( A,l,CLASS_A_calc)(l,a,b);
-
-	printf("res is %f\n",res->value.doubleVal);
-	EXPECT_EQ (5,res->value.doubleVal);
-	vdmFree(res);
-
-	TVP res2 = GET_VTABLE_FUNC( A,l,CLASS_A_sum)(l);
-	printf("res2 is %d\n",res2->value.intVal);
-	EXPECT_EQ (4,res2->value.intVal);
-	vdmFree(res2);
-
-	EXPECT_EQ (4,l->field1->value.intVal);
-
-	vdmFree(a);
-	vdmFree(b);
-	vdmFree(c);
-}
-
 TEST(A, _new)
 {
 	TVP c=A._new();
@@ -109,9 +71,6 @@ TEST(A, _new)
 	vdmFree(b);
 	vdmFree(c);
 }
-
-
-
 
 TEST(B, _new)
 {
@@ -155,7 +114,6 @@ TEST(B, _newAsA)
 
 }
 
-
 TEST(B, _newAsC)
 {
 	TVP c=B._new();
@@ -168,12 +126,11 @@ TEST(B, _newAsC)
 
 	checkFreeDouble("calculation field1c",12.34, CCCALL( B,C,l,CLASS_C_getField1);
 
-	vdmFree(a);
-	vdmFree(b);
-	vdmFree(c);
+			vdmFree(a);
+			vdmFree(b);
+			vdmFree(c);
 
-}
-
+		}
 
 TEST(C, _new)
 {
