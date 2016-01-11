cd36bc15ccf38418ee6754e31162e1a54a9fa467
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-01-11 23:08:23 +0100

    updated field rewrite rule to handle inheritance

--- src/main/java/org/overture/codegen/cgen/transformations/FieldIdentifierToFieldGetApplyTrans.java
+++ src/main/java/org/overture/codegen/cgen/transformations/FieldIdentifierToFieldGetApplyTrans.java
@@ -1,11 +1,15 @@
 package org.overture.codegen.cgen.transformations;
 
+import org.overture.ast.definitions.AClassClassDefinition;
+import org.overture.ast.definitions.AInheritedDefinition;
 import org.overture.ast.definitions.AInstanceVariableDefinition;
+import org.overture.ast.definitions.PDefinition;
 import org.overture.ast.expressions.AVariableExp;
 import org.overture.codegen.cgast.analysis.AnalysisException;
 import org.overture.codegen.cgast.analysis.DepthFirstAnalysisAdaptor;
 import org.overture.codegen.cgast.expressions.AApplyExpCG;
 import org.overture.codegen.cgast.expressions.AIdentifierVarExpCG;
+import org.overture.codegen.ir.SourceNode;
 import org.overture.codegen.trans.assistants.TransAssistantCG;
 
 public class FieldIdentifierToFieldGetApplyTrans extends
@@ -20,10 +24,12 @@ public class FieldIdentifierToFieldGetApplyTrans extends
 		this.assist = assist;
 	}
 
-	AIdentifierVarExpCG createIdentifier(String name)
+	AIdentifierVarExpCG createIdentifier(String name,
+			org.overture.ast.node.INode derrivedFrom)
 	{
 		AIdentifierVarExpCG ident = new AIdentifierVarExpCG();
 		ident.setName(name);
+		ident.setSourceNode(new SourceNode(derrivedFrom));
 		return ident;
 	}
 
@@ -34,30 +40,45 @@ public class FieldIdentifierToFieldGetApplyTrans extends
 		if (node.getIsLocal())
 			return;
 
-		if (node.getSourceNode().getVdmNode() instanceof AVariableExp
-				&& ((AVariableExp) node.getSourceNode().getVdmNode()).getVardef() instanceof AInstanceVariableDefinition)
+		if (node.getSourceNode().getVdmNode() instanceof AVariableExp)
 		{
+			AVariableExp varExp = (AVariableExp) node.getSourceNode().getVdmNode();
+
+			String thisClassName = varExp.getAncestor(AClassClassDefinition.class).getName().getName();// the containing
+																										// class
+			String fieldClassName = null;
+
+			PDefinition vardef = varExp.getVardef();
+			if (vardef instanceof AInstanceVariableDefinition)
+			{
+				fieldClassName = thisClassName;
+			} else if (vardef instanceof AInheritedDefinition)
+			{
+				AInheritedDefinition idef = (AInheritedDefinition) vardef;
+				fieldClassName = idef.getClassDefinition().getName().getName();
+			}
+
 			AApplyExpCG apply = new AApplyExpCG();
 
 			AIdentifierVarExpCG getFieldPtrMacroId = new AIdentifierVarExpCG();
 			getFieldPtrMacroId.setName("GET_FIELD_PTR");
+			getFieldPtrMacroId.setSourceNode(new SourceNode(node.getSourceNode().getVdmNode()));
 
 			apply.setRoot(getFieldPtrMacroId);
 			assist.replaceNodeWith(node, apply);
 
-			AVariableExp var = ((AVariableExp) node.getSourceNode().getVdmNode());
-			String thisClassName = var.getVardef().getClassDefinition().getName().getName();
-			String fieldClassName = thisClassName;
+			// AVariableExp var = vdmNode;
+			// String thisClassName = var.getVardef().getClassDefinition().getName().getName();
+			// String fieldClassName = thisClassName;
 
 			// add this type
-			apply.getArgs().add(createIdentifier(thisClassName));
+			apply.getArgs().add(createIdentifier(thisClassName, node.getSourceNode().getVdmNode()));
 			// add field owner type
-			apply.getArgs().add(createIdentifier(fieldClassName));
+			apply.getArgs().add(createIdentifier(fieldClassName, node.getSourceNode().getVdmNode()));
 			// add this
-			apply.getArgs().add(createIdentifier("this"));
+			apply.getArgs().add(createIdentifier("this", node.getSourceNode().getVdmNode()));
 			// add field name
 			apply.getArgs().add(node);
 		}
 	}
-
 }
