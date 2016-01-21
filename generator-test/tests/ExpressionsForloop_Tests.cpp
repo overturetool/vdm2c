2bf3c16a95f49a7b119c79ea066df8cd1fe41507
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-01-21 11:13:21 +0100

    added forloop test

---
+++ generator-test/tests/ExpressionsForloop_Tests.cpp
@@ -0,0 +1,40 @@
+/*
+ * ExpressionsSeq_Tests.cpp
+ *
+ *  Created on: Jan 10, 2016
+ *      Author: kel
+ */
+
+
+#include "gtest/gtest.h"
+
+extern "C"
+{
+#include "lib/Vdm.h"
+#include <stdio.h>
+#include "ExpressionForLoop.h"
+#ifdef VDM_CG
+#include "MethodNameMap.h"
+#endif
+}
+
+#define CHECK(methodId) TVP c=ExpressionForLoop._new();\
+TVP res=CALL_FUNC(ExpressionForLoop,ExpressionForLoop,c,methodId);\
+EXPECT_EQ (true,res->value.boolVal);\
+vdmFree(res);\
+vdmFree(c)
+
+//TEST(Expression_Seq, forindex)
+//{
+//	CHECK();
+//}
+
+TEST(Expression_Seq, forset)
+{
+	CHECK(CLASS_ExpressionForLoop__Z6forsetEV);
+}
+
+TEST(Expression_Seq, forseq)
+{
+	CHECK(CLASS_ExpressionForLoop__Z6forseqEV);
+}
