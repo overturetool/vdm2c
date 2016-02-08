c7039574f14a65f83a3d434eb19596df17be94d1
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-02-09 00:09:06 +0100

    moved method creation to util class

--- src/main/java/org/overture/codegen/vdm2c/transformations/InitializerExtractorTrans.java
+++ src/main/java/org/overture/codegen/vdm2c/transformations/InitializerExtractorTrans.java
@@ -1,6 +1,7 @@
 package org.overture.codegen.vdm2c.transformations;
 
 import static org.overture.codegen.vdm2c.utils.CTransUtil.newApply;
+import static org.overture.codegen.vdm2c.utils.CTransUtil.newInternalMethod;
 import static org.overture.codegen.vdm2c.utils.CTransUtil.newReturnStm;
 
 import org.overture.codegen.ir.SExpIR;
@@ -11,10 +12,7 @@ import org.overture.codegen.ir.analysis.DepthFirstAnalysisAdaptor;
 import org.overture.codegen.ir.declarations.AFieldDeclIR;
 import org.overture.codegen.ir.declarations.AMethodDeclIR;
 import org.overture.codegen.ir.declarations.SClassDeclIR;
-import org.overture.codegen.ir.types.AMethodTypeIR;
 import org.overture.codegen.trans.assistants.TransAssistantIR;
-import org.overture.codegen.vdm2c.Vdm2cTag;
-import org.overture.codegen.vdm2c.Vdm2cTag.MethodTag;
 import org.overture.codegen.vdm2c.utils.NameMangler;
 
 public class InitializerExtractorTrans extends DepthFirstAnalysisAdaptor
@@ -31,34 +29,22 @@ public class InitializerExtractorTrans extends DepthFirstAnalysisAdaptor
 	public void caseAFieldDeclIR(AFieldDeclIR node) throws AnalysisException
 	{
 		SExpIR initial = node.getInitial();
-		if(initial!=null)
+		if (initial != null)
 		{
-			if(initial.getType()==null)
+			if (initial.getType() == null)
 			{
 				node.setInitial(null);
-				return;}
+				return;
+			}
 			STypeIR type = initial.getType().clone();
-//			SExpIR exp = ndoe.get
 			SStmIR body = newReturnStm(initial);
-			
-			AMethodDeclIR method = new AMethodDeclIR();
-			method.setAbstract(false);
-			method.setAsync(false);
-			method.setImplicit(false);
-			method.setStatic(true);
-			method.setIsConstructor(false);
-			method.setTag(new Vdm2cTag().addMethodTag(MethodTag.Internal));
-			method.setBody(body);
-			AMethodTypeIR mtype = new AMethodTypeIR();
-			mtype.setResult(type);
-			method.setMethodType(mtype);
-			method.setName(assist.getInfo().getTempVarNameGen().nextVarName(FIELD_INITIALIZER));
-			method.setName(NameMangler.mangle(method));
-			
+
+			AMethodDeclIR method = newInternalMethod(assist.getInfo().getTempVarNameGen().nextVarName(FIELD_INITIALIZER), body, type);
+
 			node.setInitial(newApply(NameMangler.mangle(method)));
-			
+
 			SClassDeclIR cls = node.getAncestor(SClassDeclIR.class);
-			cls.getMethods().add(0,method);
+			cls.getMethods().add(0, method);
 		}
 	}
 }
