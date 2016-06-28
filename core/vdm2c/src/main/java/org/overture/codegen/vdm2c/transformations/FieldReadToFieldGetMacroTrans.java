8c2eff1233fc8b3369b3e3115dac53e9f694b052
Victor Bandur <vpb500@york.ac.uk>
2016-06-28 16:03:34 +0200

    -  First part of cleaned field access transformation. -  Read of field of local object. -  Does not work for local fields, because the nodes are of a different    type, so they must be matched separately (VarExp vs. FieldExp.)
    	modified:   core/vdm2c/NativeClassesTests.launch
    	modified:   core/vdm2c/src/main/java/org/overture/codegen/vdm2c/CTransSeries.java
    	modified:   core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/FieldReadToFieldGetMacroTrans.java
    	modified:   core/vdm2c/src/test/java/org/overture/codegen/vdm2c/NativeClassesTests.java
    	modified:   core/vdm2c/src/test/resources/vdmrt/classes/ClassPublicFieldsTMP.vdmrt

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/FieldReadToFieldGetMacroTrans.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/FieldReadToFieldGetMacroTrans.java
@@ -24,6 +24,7 @@ import org.overture.codegen.ir.analysis.AnalysisException;
 import org.overture.codegen.ir.expressions.AFieldExpIR;
 import org.overture.codegen.ir.expressions.AIdentifierVarExpIR;
 import org.overture.codegen.ir.statements.AAssignToExpStmIR;
+import org.overture.codegen.ir.types.AClassTypeIR;
 import org.overture.codegen.trans.assistants.TransAssistantIR;
 import org.overture.codegen.vdm2c.extast.expressions.AMacroApplyExpIR;
 import org.overture.codegen.vdm2c.utils.CTransUtil;
@@ -48,143 +49,245 @@ DepthFirstAnalysisCAdaptor
 		this.assist = assist;
 		this.fieldUtil = new GlobalFieldUtil(assist);
 	}
-	
-	
+
+
 	@Override
 	public void caseAFieldExpIR(AFieldExpIR node) throws AnalysisException
 	{
 		// TODO Auto-generated method stub
 		super.caseAFieldExpIR(node);
 		
-		
 		//Differentiate here between public field get and set.
-		
-	}
-	
 
-	@Override
-	public void caseAIdentifierVarExpIR(AIdentifierVarExpIR node)
-			throws AnalysisException
-	{
-		if (node.getIsLocal())
-		{
-			return;
-		}
-		
+
+		//Ensure we are not in an assignment statement.
 		if(node.parent() instanceof AAssignToExpStmIR)
 		{
-			//Assignment handled differently in its own transformation.
-			return;
-		}
-
-		String thisClassName = null;
-		String fieldClassName = null;
-
-		INode vdmNode = node.getSourceNode().getVdmNode();
-		if (vdmNode instanceof AVariableExp)
-		{
-			AVariableExp varExp = (AVariableExp) vdmNode;
-
-			if (varExp.getType() instanceof AFunctionType
-					|| varExp.getType() instanceof AOperationType)
+			if(((AAssignToExpStmIR)node.parent()).getTarget() == node)
 			{
+				//Current field expression is the target of an assignment.  This is handled elsewhere.
 				return;
 			}
-
-			thisClassName = varExp.getAncestor(AClassClassDefinition.class).getName().getName();// the containing
-			// class
-			fieldClassName = thisClassName; // default to same class
-
-			if (varExp.getVardef() instanceof AInheritedDefinition)
-			{
-				AInheritedDefinition idef = (AInheritedDefinition) varExp.getVardef();
-				fieldClassName = idef.getClassDefinition().getName().getName();
-			}
-
-			PDefinition vardef = CTransUtil.unwrapInheritedDef(varExp.getVardef());
-
-			if (vardef instanceof AInstanceVariableDefinition)
-			{
-				if (vardef.getAccess().getStatic() != null)
-				{
-					fieldUtil.replaceWithStaticReference(vardef.getClassDefinition(), node);
-					return;
-				}
-
-			} else if (vardef instanceof ALocalDefinition
-					&& ((ALocalDefinition) vardef).getValueDefinition())
-			{
-				if (vardef.getAccess().getStatic() != null)
-				{
-					fieldUtil.replaceWithStaticReference(vardef.getClassDefinition(), node);
-					return;
-				}
-				return;
-			}
-
-		} else if (vdmNode instanceof AIdentifierStateDesignator)
-		{
-			AIdentifierStateDesignator designator = (AIdentifierStateDesignator) vdmNode;
-
-			// why is the definition not kept here? we dont know what this points to
-
-			
-			SClassDefinitionBase thisClass = designator.getAncestor(AClassClassDefinition.class);
-			
-			if(thisClass == null)
-			{
-				//Must be a system class.
-				thisClass = designator.getAncestor(ASystemClassDefinition.class);
-			}
-							
-			 
-			 // the containing class
-			thisClassName = thisClass.getName().getName();
-
-			// while (thisClass != null && fieldClassName == null)
-			{
-				List<PDefinition> definitons = new Vector<PDefinition>();
-				definitons.addAll(thisClass.getDefinitions());
-				definitons.addAll(thisClass.getAllInheritedDefinitions());
-				for (PDefinition def : definitons)
-				{
-					if (def instanceof AInheritedDefinition)
-					{
-						def = ((AInheritedDefinition) def).getSuperdef();
-					}
-
-					if (def instanceof AInstanceVariableDefinition)
-					{
-						if (def.getName().getName().equals(designator.getName().getName()))
-						{
-							fieldClassName = def.getClassDefinition().getName().getName();
-							break;
-						}
-					}
-				}
-			}
-
-			if (fieldClassName == null)
-			{
-				logger.warn("Field class name not found: {}", node);
-			}
 		}
 
-		if (thisClassName != null && fieldClassName != null)
-		{
+		//The remainder of the transformation does not yet deal with inherited field definitions.
+//		String thisClassName = node.getAncestor(SClassDeclIR.class).getName();
+		String fieldClassName = ((AClassTypeIR)((AIdentifierVarExpIR)node.getObject()).getType()).getName();
+		AFieldExpIR tmpnode = node.clone();
 
-			AMacroApplyExpIR apply = newMacroApply(GET_FIELD_PTR);
-			assist.replaceNodeWith(node, apply);
+		AMacroApplyExpIR apply = newMacroApply(GET_FIELD_PTR);
+		assist.replaceNodeWith(node, apply);
 
-			// add this type
-			apply.getArgs().add(createIdentifier(thisClassName, node.getSourceNode()));
-			// add field owner type
-			apply.getArgs().add(createIdentifier(fieldClassName, node.getSourceNode()));
-			// add this
-			apply.getArgs().add(createIdentifier("this", node.getSourceNode()));
-			// add field name
-			apply.getArgs().add(node);
-			apply.setType(node.getType());
-		}
+		// add this type
+		apply.getArgs().add(createIdentifier(fieldClassName, tmpnode.getSourceNode()));
+		// add field owner type
+		apply.getArgs().add(createIdentifier(fieldClassName, tmpnode.getSourceNode()));
+		// add this
+		apply.getArgs().add(createIdentifier("this", tmpnode.getSourceNode()));
+		// add field name
+		apply.getArgs().add(createIdentifier(tmpnode.getMemberName(), tmpnode.getSourceNode()));
+		apply.setType(node.getType());
+
+
+
+
+//		if (vdmNode instanceof AVariableExp)
+//		{
+//			AVariableExp varExp = (AVariableExp) vdmNode;
+//
+//			if (varExp.getType() instanceof AFunctionType
+//					|| varExp.getType() instanceof AOperationType)
+//			{
+//				return;
+//			}
+//
+//			thisClassName = varExp.getAncestor(AClassClassDefinition.class).getName().getName();// the containing
+//			// class
+//			fieldClassName = thisClassName; // default to same class
+//
+//			if (varExp.getVardef() instanceof AInheritedDefinition)
+//			{
+//				AInheritedDefinition idef = (AInheritedDefinition) varExp.getVardef();
+//				fieldClassName = idef.getClassDefinition().getName().getName();
+//			}
+//
+//			PDefinition vardef = CTransUtil.unwrapInheritedDef(varExp.getVardef());
+//
+//			//			if 
+//
+//		} else if (vdmNode instanceof AIdentifierStateDesignator)
+//		{
+//			AIdentifierStateDesignator designator = (AIdentifierStateDesignator) vdmNode;
+//
+//			// why is the definition not kept here? we dont know what this points to
+//
+//
+//			SClassDefinitionBase thisClass = designator.getAncestor(AClassClassDefinition.class);
+//
+//			if(thisClass == null)
+//			{
+//				//Must be a system class.
+//				thisClass = designator.getAncestor(ASystemClassDefinition.class);
+//			}
+//
+//
+//			// the containing class
+//			thisClassName = thisClass.getName().getName();
+//
+//			// while (thisClass != null && fieldClassName == null)
+//			{
+//				List<PDefinition> definitons = new Vector<PDefinition>();
+//				definitons.addAll(thisClass.getDefinitions());
+//				definitons.addAll(thisClass.getAllInheritedDefinitions());
+//				for (PDefinition def : definitons)
+//				{
+//					if (def instanceof AInheritedDefinition)
+//					{
+//						def = ((AInheritedDefinition) def).getSuperdef();
+//					}
+//
+//					if (def instanceof AInstanceVariableDefinition)
+//					{
+//						if (def.getName().getName().equals(designator.getName().getName()))
+//						{
+//							fieldClassName = def.getClassDefinition().getName().getName();
+//							break;
+//						}
+//					}
+//				}
+//			}
+//
+//			if (fieldClassName == null)
+//			{
+//				logger.warn("Field class name not found: {}", node);
+//			}
+//		}	
 	}
+
+
+//		@Override
+//		public void caseAIdentifierVarExpIR(AIdentifierVarExpIR node)
+//				throws AnalysisException
+//		{
+//			if (node.getIsLocal())
+//			{
+//				return;
+//			}
+//			
+//			if(node.parent() instanceof AAssignToExpStmIR)
+//			{
+//				//Assignment handled differently in its own transformation.
+//				return;
+//			}
+//	
+//			String thisClassName = null;
+//			String fieldClassName = null;
+//	
+//			INode vdmNode = node.getSourceNode().getVdmNode();
+//			if (vdmNode instanceof AVariableExp)
+//			{
+//				AVariableExp varExp = (AVariableExp) vdmNode;
+//	
+//				if (varExp.getType() instanceof AFunctionType
+//						|| varExp.getType() instanceof AOperationType)
+//				{
+//					return;
+//				}
+//	
+//				thisClassName = varExp.getAncestor(AClassClassDefinition.class).getName().getName();// the containing
+//				// class
+//				fieldClassName = thisClassName; // default to same class
+//	
+//				if (varExp.getVardef() instanceof AInheritedDefinition)
+//				{
+//					AInheritedDefinition idef = (AInheritedDefinition) varExp.getVardef();
+//					fieldClassName = idef.getClassDefinition().getName().getName();
+//				}
+//	
+//				PDefinition vardef = CTransUtil.unwrapInheritedDef(varExp.getVardef());
+//	
+//				if (vardef instanceof AInstanceVariableDefinition)
+//				{
+//					if (vardef.getAccess().getStatic() != null)
+//					{
+//						fieldUtil.replaceWithStaticReference(vardef.getClassDefinition(), node);
+//						return;
+//					}
+//	
+//				} else if (vardef instanceof ALocalDefinition
+//						&& ((ALocalDefinition) vardef).getValueDefinition())
+//				{
+//					if (vardef.getAccess().getStatic() != null)
+//					{
+//						fieldUtil.replaceWithStaticReference(vardef.getClassDefinition(), node);
+//						return;
+//					}
+//					return;
+//				}
+//	
+//			} else if (vdmNode instanceof AIdentifierStateDesignator)
+//			{
+//				AIdentifierStateDesignator designator = (AIdentifierStateDesignator) vdmNode;
+//	
+//				// why is the definition not kept here? we dont know what this points to
+//	
+//				
+//				SClassDefinitionBase thisClass = designator.getAncestor(AClassClassDefinition.class);
+//				
+//				if(thisClass == null)
+//				{
+//					//Must be a system class.
+//					thisClass = designator.getAncestor(ASystemClassDefinition.class);
+//				}
+//								
+//				 
+//				 // the containing class
+//				thisClassName = thisClass.getName().getName();
+//	
+//				// while (thisClass != null && fieldClassName == null)
+//				{
+//					List<PDefinition> definitons = new Vector<PDefinition>();
+//					definitons.addAll(thisClass.getDefinitions());
+//					definitons.addAll(thisClass.getAllInheritedDefinitions());
+//					for (PDefinition def : definitons)
+//					{
+//						if (def instanceof AInheritedDefinition)
+//						{
+//							def = ((AInheritedDefinition) def).getSuperdef();
+//						}
+//	
+//						if (def instanceof AInstanceVariableDefinition)
+//						{
+//							if (def.getName().getName().equals(designator.getName().getName()))
+//							{
+//								fieldClassName = def.getClassDefinition().getName().getName();
+//								break;
+//							}
+//						}
+//					}
+//				}
+//	
+//				if (fieldClassName == null)
+//				{
+//					logger.warn("Field class name not found: {}", node);
+//				}
+//			}
+//	
+//			if (thisClassName != null && fieldClassName != null)
+//			{
+//	
+//				AMacroApplyExpIR apply = newMacroApply(GET_FIELD_PTR);
+//				assist.replaceNodeWith(node, apply);
+//	
+//				// add this type
+//				apply.getArgs().add(createIdentifier(thisClassName, node.getSourceNode()));
+//				// add field owner type
+//				apply.getArgs().add(createIdentifier(fieldClassName, node.getSourceNode()));
+//				// add this
+//				apply.getArgs().add(createIdentifier("this", node.getSourceNode()));
+//				// add field name
+//				apply.getArgs().add(node);
+//				apply.setType(node.getType());
+//			}
+//		}
 }
