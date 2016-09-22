8e0b271d6df09cde233e949157029560daf8a8d1
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-09-22 16:28:58 +0200

    Test that issue with code generation of static field access is fixed
    Related to issue #12

---
+++ core/vdm2c/src/test/resources/native/classes/ClassStaticInstanceVariableComplex_Tests.cpp
@@ -0,0 +1,38 @@
+/*
+ * ClassStaticMethodAccess_Tests.cpp
+ *
+ *  Created on: Jan 10, 2016
+ *      Author: Victor Bandur
+ */
+
+
+#include "gtest/gtest.h"
+
+extern "C"
+{
+#include "Vdm.h"
+#include <stdio.h>
+#include "B.h"
+}
+
+#define CHECK(methodId)\
+TVP b=_Z1BEV(NULL);\
+TVP res=CALL_FUNC(B,B,b,methodId);\
+EXPECT_EQ (true,res->value.boolVal);\
+vdmFree(res);\
+vdmFree(b)
+
+
+TEST(ClassStaticInstanceVariableComplex, testInitialisedField)
+{
+	A_static_init();
+	CHECK(CLASS_B__Z20testInitialisedFieldEV);
+}
+
+TEST(ClassStaticInstanceVariableComplex, testUpdatedField)
+{
+	A_static_init();
+	CHECK(CLASS_B__Z16testUpdatedFieldEV);
+}
+
+
