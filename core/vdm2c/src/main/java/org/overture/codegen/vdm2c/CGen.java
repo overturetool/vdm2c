328af1c3a6ec2c6bb7285e9a54c87e35a6dad7b6
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-09-29 15:20:58 +0200

    Convert record declarations to class declarations

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/CGen.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/CGen.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
@@ -13,21 +14,30 @@ import org.apache.commons.lang.time.StopWatch;
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.codegen.ir.CodeGenBase;
 import org.overture.codegen.ir.INode;
+import org.overture.codegen.ir.IRConstants;
 import org.overture.codegen.ir.IRStatus;
 import org.overture.codegen.ir.IrNodeInfo;
 import org.overture.codegen.ir.PIR;
+import org.overture.codegen.ir.VdmNodeInfo;
 import org.overture.codegen.ir.analysis.DepthFirstAnalysisAdaptor;
 import org.overture.codegen.ir.declarations.ADefaultClassDeclIR;
 import org.overture.codegen.ir.declarations.AFieldDeclIR;
+import org.overture.codegen.ir.declarations.AFormalParamLocalParamIR;
 import org.overture.codegen.ir.declarations.AMethodDeclIR;
+import org.overture.codegen.ir.declarations.ARecordDeclIR;
 import org.overture.codegen.ir.declarations.ASystemClassDeclIR;
+import org.overture.codegen.ir.declarations.ATypeDeclIR;
 import org.overture.codegen.ir.declarations.SClassDeclIR;
+import org.overture.codegen.ir.expressions.AIdentifierVarExpIR;
 import org.overture.codegen.ir.name.ATokenNameIR;
+import org.overture.codegen.ir.statements.ABlockStmIR;
 import org.overture.codegen.ir.types.AClassTypeIR;
+import org.overture.codegen.ir.types.AMethodTypeIR;
 import org.overture.codegen.utils.GeneratedData;
 import org.overture.codegen.utils.GeneratedModule;
 import org.overture.codegen.vdm2c.extast.declarations.AClassHeaderDeclIR;
 import org.overture.codegen.vdm2c.sourceformat.ISourceFileFormatter;
+import org.overture.codegen.vdm2c.utils.CTransUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,18 +51,118 @@ public class CGen extends CodeGenBase
 	{
 		this.outputFolder = outputFolder;
 	}
+	
+	public List<IRStatus<PIR>> makeRecsOuterClasses(List<IRStatus<PIR>> ast)
+	{
+		List<IRStatus<PIR>> extraClasses = new LinkedList<IRStatus<PIR>>();
 
+		for (IRStatus<ADefaultClassDeclIR> status : IRStatus.extract(ast, ADefaultClassDeclIR.class))
+		{
+			ADefaultClassDeclIR clazz = status.getIrNode();
+
+			List<ARecordDeclIR> recDecls = new LinkedList<ARecordDeclIR>();
+
+			for (ATypeDeclIR d : clazz.getTypeDecls())
+			{
+				if (d.getDecl() instanceof ARecordDeclIR)
+				{
+					recDecls.add((ARecordDeclIR) d.getDecl());
+				}
+			}
+
+			// Note that we do not remove the type declarations (or records) from the class.
+
+			// For each of the records we will make a top-level class
+			for (ARecordDeclIR recDecl : recDecls)
+			{
+				ADefaultClassDeclIR recClass = new ADefaultClassDeclIR();
+
+				recClass.setMetaData(recDecl.getMetaData());
+				recClass.setAbstract(false);
+				recClass.setAccess(IRConstants.PUBLIC);
+				recClass.setSourceNode(recDecl.getSourceNode());
+				recClass.setStatic(false);
+				recClass.setName(recDecl.getName());
+
+				if (recDecl.getInvariant() != null)
+				{
+					recClass.setInvariant(recDecl.getInvariant().clone());
+				}
+
+				// Copy the record fields to the class
+				List<AFieldDeclIR> fields = new LinkedList<AFieldDeclIR>();
+				for (AFieldDeclIR f : recDecl.getFields())
+				{
+					AFieldDeclIR newField = f.clone();
+					fields.add(newField);
+				}
+				
+				recClass.setFields(fields);
+				
+				AClassTypeIR retType = new AClassTypeIR();
+				retType.setName(recClass.getName());
+				
+				AMethodTypeIR ctorMethodType = new AMethodTypeIR();
+				ctorMethodType.setResult(retType);
+				
+				ABlockStmIR ctorBody = new ABlockStmIR();
+				
+				AMethodDeclIR ctor = new AMethodDeclIR();
+				ctor.setIsConstructor(true);
+				ctor.setAbstract(false);
+				ctor.setAccess(IRConstants.PUBLIC);
+				ctor.setName(recClass.getName());
+				ctor.setBody(ctorBody);
+				
+				
+				ctor.setMethodType(ctorMethodType);
+				
+				for(AFieldDeclIR f : recClass.getFields())
+				{
+					AFormalParamLocalParamIR param = new AFormalParamLocalParamIR();
+					String name = "param_" + f.getName();
+					param.setPattern(CTransUtil.newIdentifierPattern(name));
+					param.setType(f.getType().clone());
+					
+					ctor.getFormalParams().add(param);
+					ctorMethodType.getParams().add(f.getType().clone());
+					
+					AIdentifierVarExpIR target = CTransUtil.newIdentifier(f.getName(), f.getSourceNode());
+					target.setType(f.getType().clone());
+					target.setIsLocal(false);
+					
+					AIdentifierVarExpIR assignVal = CTransUtil.newIdentifier(name, f.getSourceNode());
+					assignVal.setType(f.getType().clone());
+					
+					ctorBody.getStatements().add(CTransUtil.newAssignment(
+							target, assignVal));	
+				}
+				
+				recClass.getMethods().add(ctor);
+
+				extraClasses.add(new IRStatus<PIR>(recClass.getSourceNode().getVdmNode(), recClass.getName(), recClass, new HashSet<VdmNodeInfo>()));
+			}
+		}
+
+		return extraClasses;
+	}
+	
 	@Override
 	protected GeneratedData genVdmToTargetLang(List<IRStatus<PIR>> statuses)
 			throws AnalysisException
 	{
 		statuses = replaceSystemClassWithClass(statuses);
 		statuses = ignoreVDMUnitTests(statuses);
+		List<IRStatus<PIR>> recClasses = makeRecsOuterClasses(statuses);
+		statuses.addAll(recClasses);
+		
+		for(IRStatus<ADefaultClassDeclIR> r : IRStatus.extract(recClasses, ADefaultClassDeclIR.class))
+		{
+			getInfo().getClasses().add(r.getIrNode());
+		}
 
 		generateClassHeaders(statuses);
 		applyTransformations(statuses);
-		
-
 
 		VTableGenerator.generate(IRStatus.extract(statuses, AClassHeaderDeclIR.class));
 
