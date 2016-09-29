ac9d528bfc9e1dc59536c9abd4df19e3672d99d9
Victor Bandur <vpb500@york.ac.uk>
2016-09-29 12:07:37 +0200

    -  Expanded override test to two-level inheritance. -  Addresses #8.
    	modified:   core/vdm2c/src/test/resources/native/classes/ClassOpOverride_Tests.cpp
    	modified:   core/vdm2c/src/test/resources/vdmrt/classes/ClassOpOverride.vdmrt

--- core/vdm2c/src/test/resources/native/classes/ClassOpOverride_Tests.cpp
+++ core/vdm2c/src/test/resources/native/classes/ClassOpOverride_Tests.cpp
@@ -13,18 +13,28 @@ extern "C"
 #include "Vdm.h"
 #include <stdio.h>
 #include "ClassOpOverride1.h"
+#include "ClassOpOverride2.h"
 }
 
-#define CHECK(methodId) TVP c=_Z16ClassOpOverride1EV(NULL);\
+#define CHECK_TRUE(methodId) TVP c=_Z16ClassOpOverride1EV(NULL);\
 TVP res=CALL_FUNC(ClassOpOverride1, ClassOpOverride1, c, methodId);\
 EXPECT_EQ (true, res->value.boolVal);\
 vdmFree(res);\
 vdmFree(c)
 
+#define CHECK_FALSE(methodId) TVP c=_Z16ClassOpOverride2EV(NULL);\
+TVP res=CALL_FUNC(ClassOpOverride2, ClassOpOverride2, c, methodId);\
+EXPECT_EQ (false, res->value.boolVal);\
+vdmFree(res);\
+vdmFree(c)
 
 
 TEST(ClassOpOverride, op)
 {
-	CHECK(CLASS_ClassOpOverride1__Z2opEV);
+	CHECK_TRUE(CLASS_ClassOpOverride1__Z2opEV);
 }
 
+TEST(ClassOpOverride, second_op)
+{
+	CHECK_FALSE(CLASS_ClassOpOverride2__Z2opEV);
+}
