1ec2821fa5e03d8a586b6228a2429924c617e12a
Miran <miranh89@gmail.com>
2016-10-12 13:08:51 +0200

    Minor updates to a test of distributed transformation

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/distribution/transformations/DistTransTest.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/distribution/transformations/DistTransTest.java
@@ -2,7 +2,7 @@ package org.overture.codegen.vdm2c.distribution.transformations;
 
 import org.overture.cgc.extast.analysis.DepthFirstAnalysisCAdaptor;
 import org.overture.codegen.ir.analysis.AnalysisException;
-import org.overture.codegen.ir.declarations.AMethodDeclIR;
+import org.overture.codegen.ir.declarations.ADefaultClassDeclIR;
 import org.overture.codegen.trans.assistants.TransAssistantIR;
 
 public class DistTransTest extends DepthFirstAnalysisCAdaptor
@@ -12,10 +12,32 @@ public class DistTransTest extends DepthFirstAnalysisCAdaptor
 	{
 	}
 
+//	@Override
+//	public void caseAMethodDeclIR(AMethodDeclIR node) throws AnalysisException
+//	{
+//		System.out.println("Dist transformation, method name: " + node.getName());
+//	}
+//	
 	@Override
-	public void caseAMethodDeclIR(AMethodDeclIR node) throws AnalysisException
+	public void caseADefaultClassDeclIR(ADefaultClassDeclIR node)
+			throws AnalysisException
 	{
-		System.out.println("Dist transformation, method name: " + node.getName());
+		
+		//AClassHeaderDeclIR par = node.getAncestor(AClassHeaderDeclIR.class);
+		//par.apply(this);
+		//System.out.println("Class name: " + node.getName());
 	}
+	
+//	@Override
+//	public void caseAArrayDeclIR(AArrayDeclIR node) throws AnalysisException
+//	{
+//		// TODO Auto-generated method stub
+//		super.caseAArrayDeclIR(node);
+//		
+//		System.out.println(node.getName());
+//		
+//		
+//	}
+	
 
 }
