935d11a29e93a9554402feb0b10dfd6c2cc1f83d
Victor Bandur <vpb500@york.ac.uk>
2016-10-18 13:06:08 +0200

    Improved makefile.

--- c/vdmclib/src/test/memperf/memperf.c
+++ c/vdmclib/src/test/memperf/memperf.c
@@ -143,7 +143,6 @@ void testNewSetWithValues()
 	theSet = newSetWithValues(numElems, elems);
 	
 	vdmFree(theSet);
-	//vdmFree for sets seems to not free the top-level pointer, this might be the reason for the leak.  May also explain the 8-byte per item loss, size of struct Collection?
 
 	for(elem = 0;  elem < numElems;  elem++)
 	{
