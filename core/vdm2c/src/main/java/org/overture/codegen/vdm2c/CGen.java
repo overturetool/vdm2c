4f7ba9e3c71de78ed8909205e9f2dadc4b46f63c
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-11-21 13:04:55 +0100

    Represent the generated code using CGP means
    Intermediate work on #41

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/CGen.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/CGen.java
@@ -4,7 +4,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,7 +13,6 @@ import java.util.Vector;
 import org.apache.commons.lang.time.StopWatch;
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.codegen.ir.CodeGenBase;
-import org.overture.codegen.ir.INode;
 import org.overture.codegen.ir.IRConstants;
 import org.overture.codegen.ir.IRStatus;
 import org.overture.codegen.ir.IrNodeInfo;
@@ -163,6 +161,8 @@ public class CGen extends CodeGenBase
 	protected GeneratedData genVdmToTargetLang(List<IRStatus<PIR>> statuses)
 			throws AnalysisException
 	{
+		List<GeneratedModule> genModules = new LinkedList<GeneratedModule>();
+		
 		statuses = replaceSystemClassWithClass(statuses);
 		statuses = ignoreVDMUnitTests(statuses);
 		List<IRStatus<PIR>> recClasses = makeRecsOuterClasses(statuses);
@@ -172,8 +172,21 @@ public class CGen extends CodeGenBase
 		{
 			getInfo().getClasses().add(r.getIrNode());
 		}
+		
+		List<IRStatus<PIR>> canBeGenerated = new LinkedList<>();
+		
+		for (IRStatus<PIR> status : statuses)
+		{
+			if (status.canBeGenerated())
+			{
+				canBeGenerated.add(status);
+			} else
+			{
+				genModules.add(new GeneratedModule(status.getIrNodeName(), status.getUnsupportedInIr(), new HashSet<IrNodeInfo>(), isTestCase(status)));
+			}
+		}
 
-		for(IRStatus<ADefaultClassDeclIR> r : IRStatus.extract(statuses, ADefaultClassDeclIR.class))
+		for(IRStatus<ADefaultClassDeclIR> r : IRStatus.extract(canBeGenerated, ADefaultClassDeclIR.class))
 		{
 			try
 			{
@@ -184,22 +197,37 @@ public class CGen extends CodeGenBase
 			}
 		}
 		
-		generateClassHeaders(statuses);
-		applyTransformations(statuses);
+		generateClassHeaders(canBeGenerated);
+		applyTransformations(canBeGenerated);
 
-		VTableGenerator.generate(IRStatus.extract(statuses, AClassHeaderDeclIR.class));
+		VTableGenerator.generate(IRStatus.extract(canBeGenerated, AClassHeaderDeclIR.class));
 
-		CFormat my_formatter = consFormatter(statuses);
-		writeHeaders(outputFolder, statuses, my_formatter);
-		writeClasses(outputFolder, statuses, my_formatter);
+		CFormat my_formatter = consFormatter(canBeGenerated);
+		
+		
+		for (IRStatus<PIR> status : canBeGenerated)
+		{
+			org.overture.ast.node.INode vdmClass = status.getVdmNode();
 
-		/**
-		 * FIXME: PVJ: This method does not return the generated data as it should. Instead the method writes the
-		 * generated code to the file system and returns an empty data structure. The date structure below is empty!
-		 */
-		List<GeneratedModule> generated = new LinkedList<GeneratedModule>();
+			try
+			{
+				if (shouldGenerateVdmNode(vdmClass))
+				{
+					genModules.add(genIrModule(my_formatter.GetMergeVisitor(), status));
+
+				}
+
+			} catch (org.overture.codegen.ir.analysis.AnalysisException e)
+			{
+				log.error("Error generating code for class "
+						+ status.getIrNodeName() + ": " + e.getMessage());
+				log.error("Skipping class..");
+				e.printStackTrace();
+			}
+		}
+		
 		GeneratedData data = new GeneratedData();
-		data.setClasses(generated);
+		data.setClasses(genModules);
 
 		return data;
 	}
@@ -346,61 +374,6 @@ public class CGen extends CodeGenBase
 		return my_formatter;
 	}
 
-	public void writeClasses(File outputFolder,
-			final List<IRStatus<PIR>> statuses, CFormat my_formatter)
-	{
-		logger.debug("Writing C source files");
-		final StopWatch stopwatch = new StopWatch();
-		final StopWatch stopwatchClass = new StopWatch();
-		stopwatch.start();
-		for (IRStatus<ADefaultClassDeclIR> status : IRStatus.extract(statuses, ADefaultClassDeclIR.class))
-		{
-			// StringWriter writer = new StringWriter();
-			ADefaultClassDeclIR classCg = status.getIrNode();
-
-			try
-			{
-				stopwatchClass.reset();
-				stopwatchClass.start();
-				// logger.trace("Emitting code and writing: {}", classCg.getName());
-				writeFile(classCg, classCg.getName(), "c", my_formatter, outputFolder);
-				stopwatchClass.stop();
-				logger.trace("Emitted code for class: {} in: {}", classCg.getName(), stopwatchClass);
-			} catch (org.overture.codegen.ir.analysis.AnalysisException e1)
-			{
-				e1.printStackTrace();
-			} catch (IOException e1)
-			{
-				e1.printStackTrace();
-			}
-
-		}
-		stopwatch.stop();
-		logger.debug("Writing C source files. Completed in: {}", stopwatch);
-	}
-
-	public void writeHeaders(File outputFolder,
-			final List<IRStatus<PIR>> statuses, CFormat my_formatter)
-	{
-		for (IRStatus<AClassHeaderDeclIR> status : IRStatus.extract(statuses, AClassHeaderDeclIR.class))
-		{
-			// StringWriter writer = new StringWriter();
-			AClassHeaderDeclIR header = status.getIrNode();
-			try
-			{
-				writeFile(header, header.getName(), "h", my_formatter, outputFolder);
-			} catch (org.overture.codegen.ir.analysis.AnalysisException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-
 	public void generateClassHeaders(final List<IRStatus<PIR>> statuses)
 	{
 		try
@@ -413,57 +386,26 @@ public class CGen extends CodeGenBase
 		}
 	}
 
-	private void writeFile(INode node, String name, String extension,
-			CFormat my_formatter, File output_dir)
+	//TODO: PVJ use the emitCode method
+	public void writeFile(GeneratedModule module, String extension, File output_dir)
 					throws org.overture.codegen.ir.analysis.AnalysisException,
 					IOException
 	{
-		StringWriter writer = emitCode(node, my_formatter);
-
 		output_dir.mkdirs();
 
-		String fileName = name + (extension == null ? "" : "." + extension);
+		String fileName = module.getName() + (extension == null ? "" : "." + extension);
 
 		File file = new File(output_dir, fileName);
 		BufferedWriter output = new BufferedWriter(new FileWriter(file));
-		output.write(writer.toString());
+		output.write(module.getContent());
 		output.close();
 
 		if(formatter!=null)
 		{
 			formatter.format(file);
 		}
-
 	}
-
-	private StringWriter emitCode(INode node, CFormat my_formatter)
-			throws org.overture.codegen.ir.analysis.AnalysisException
-	{
-		StringWriter writer = new StringWriter();
-		my_formatter.GetMergeVisitor().init();
-		node.apply(my_formatter.GetMergeVisitor(), writer);// Why StringWriter?
-		if (my_formatter.GetMergeVisitor().hasMergeErrors())
-		{
-			// throw new
-			// org.overture.codegen.ir.analysis.AnalysisException(my_formatter.GetMergeVisitor().getMergeErrors().get(0));
-			for (Exception e : my_formatter.GetMergeVisitor().getMergeErrors())
-			{
-				logger.error("Merge error:", e);
-			}
-
-		}
-		if (my_formatter.GetMergeVisitor().hasUnsupportedTargLangNodes())
-		{
-			for (IrNodeInfo n : my_formatter.GetMergeVisitor().getUnsupportedInTargLang())
-			{
-				logger.warn("Merge reached unsupported template: {}", n);
-			}
-
-			// throw new
-			// org.overture.codegen.ir.analysis.AnalysisException(my_formatter.GetMergeVisitor().getUnsupportedInTargLang().iterator().next().toString());
-		}
-		return writer;
-	}
+	
 
 	/**
 	 * Generic filter method for AST lists. It works both up and down.
