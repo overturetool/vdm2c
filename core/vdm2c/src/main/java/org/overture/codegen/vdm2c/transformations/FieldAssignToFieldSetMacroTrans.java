60d8fc0654115d2c602e4168e723d2031013d64c
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-11-29 13:52:45 +0100

    Use 'ValueSemantics' class to generate clone calls

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/FieldAssignToFieldSetMacroTrans.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/FieldAssignToFieldSetMacroTrans.java
@@ -111,7 +111,7 @@ DepthFirstAnalysisCAdaptor
 
 			// process right side of assignment
 			node.getExp().apply(THIS);
-			AVarDeclIR retVar = newDeclarationAssignment(name, node.getExp().getType().clone(), CTransUtil.newApply("vdmClone", node.getExp().clone()), node.getExp().getSourceNode());
+			AVarDeclIR retVar = newDeclarationAssignment(name, node.getExp().getType().clone(), ValueSemantics.clone(node.getExp().clone()), node.getExp().getSourceNode());
 
 			ABlockStmIR replBlock = new ABlockStmIR();
 			replBlock.setScoped(true);
@@ -178,7 +178,7 @@ DepthFirstAnalysisCAdaptor
 							NameConverter.getCName(fieldUtil.lookupField(cDef, node.getTarget().toString())), null),
 							newIdentifier(name, null));
 
-			AVarDeclIR retVar = newDeclarationAssignment(name, node.getExp().getType().clone(), CTransUtil.newApply("vdmClone", node.getExp().clone()), node.getExp().getSourceNode());
+			AVarDeclIR retVar = newDeclarationAssignment(name, node.getExp().getType().clone(), ValueSemantics.clone(node.getExp().clone()), node.getExp().getSourceNode());
 
 			ABlockStmIR replBlock = new ABlockStmIR();
 			replBlock.setScoped(true);
@@ -231,7 +231,7 @@ DepthFirstAnalysisCAdaptor
 							NameConverter.getCName(fieldUtil.lookupField(cDef, fieldName)), null),
 							newIdentifier(name, null));
 
-			AVarDeclIR retVar = newDeclarationAssignment(name, node.getExp().getType().clone(), CTransUtil.newApply("vdmClone", node.getExp().clone()), node.getExp().getSourceNode());
+			AVarDeclIR retVar = newDeclarationAssignment(name, node.getExp().getType().clone(), ValueSemantics.clone(node.getExp().clone()), node.getExp().getSourceNode());
 
 			ABlockStmIR replBlock = new ABlockStmIR();
 			replBlock.setScoped(true);
