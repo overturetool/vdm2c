900a6d7727963565d9698672041fdcc162e8cdba
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-12-01 10:30:04 +0100

    Check that static instance variables do not leak

--- core/vdm2c/src/test/resources/native/explicit-memory-management/SimpleCases_Tests.cpp
+++ core/vdm2c/src/test/resources/native/explicit-memory-management/SimpleCases_Tests.cpp
@@ -9,8 +9,17 @@ extern "C"
 }
 
 // Check that values do not leak
-TEST(SimpleCases, initShutdown)
+TEST(SimpleCases, values)
 {
 	SimpleCases_const_init();
 	SimpleCases_const_shutdown();
 }
+
+// Check that static instance variables do not leak
+TEST(SimpleCases, statics)
+{
+	SimpleCases_static_init();
+	SimpleCases_static_shutdown();
+}
+
+
