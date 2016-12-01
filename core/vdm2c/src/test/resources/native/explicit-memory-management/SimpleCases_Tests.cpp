9b76db403aefba625442161a76c32107cc4e0ee2
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-12-01 11:37:23 +0100

    Check that instance variables do not leak memory

--- core/vdm2c/src/test/resources/native/explicit-memory-management/SimpleCases_Tests.cpp
+++ core/vdm2c/src/test/resources/native/explicit-memory-management/SimpleCases_Tests.cpp
@@ -22,4 +22,21 @@ TEST(SimpleCases, statics)
 	SimpleCases_static_shutdown();
 }
 
+// Check that instance variables do not leak when invoking the default
+// constructor
+TEST(SimpleCases,emptyConstructor)
+{
+  TVP c = _Z11SimpleCasesEV(NULL);
+  vdmFree(c);
+}
+
+// Check that instance variables do not leak when invoking a
+// parameterised constructor
+TEST(SimpleCases,parameterisedConstructor)
+{
+  TVP val = newInt(42);
+  TVP c = _Z11SimpleCasesEI(NULL, val);
+  vdmFree(c);
+  vdmFree(val);
+}
 
