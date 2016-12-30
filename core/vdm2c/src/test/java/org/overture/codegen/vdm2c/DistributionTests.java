cc7a74611b00c64d5707181d88c5a3fb41c16e17
Miran <miranh89@gmail.com>
2016-12-30 08:43:02 +0100

    Added test of asn serialisation of arguments and return type

--- core/vdm2c/src/test/java/org/overture/codegen/vdm2c/DistributionTests.java
+++ core/vdm2c/src/test/java/org/overture/codegen/vdm2c/DistributionTests.java
@@ -146,4 +146,86 @@ public class DistributionTests extends DistTestBase
 		cmakeUtil.run(cpu1Dir, "cpu1Exe", TEST_OUTPUT != null);
 
 	}
+
+	@Test
+	public void TestAsnProd() throws IOException, InterruptedException,
+	CMakeGenerateException
+	{
+
+		generate(getPath("dist/distProd.vdmrt"));
+
+		/** 1. Create the directory  **/
+
+		// cpu1 directory
+		File cpu1Dir = new File("/Users/Miran/Documents/C_codegen/vdm2c/core/vdm2c/target/test-cgen/DistributionTests/testAsnProd/cpu1");
+		copyTestFilesDist(cpu1Dir , new File("src/test/resources/distribution/testAsnProd/cpu1/distCall.h".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/distCall.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/CMakeLists.txt".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/main.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/intVal.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/intVal.h".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/asn1crt.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/asn1crt.h".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/real.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/serialise.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/serialise.h".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/asn1vdm.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu1/asn1vdm.h".replace('/', File.separatorChar)));
+
+		// cpu2 directory
+		File cpu2Dir = new File("/Users/Miran/Documents/C_codegen/vdm2c/core/vdm2c/target/test-cgen/DistributionTests/testAsnProd/cpu2");
+		copyTestFilesDist(cpu2Dir , new File("src/test/resources/distribution/testAsnProd/cpu2/distCall.h".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/distCall.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/CMakeLists.txt".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/main.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/intVal.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/intVal.h".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/asn1crt.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/asn1crt.h".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/real.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/serialise.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/serialise.h".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/asn1vdm.c".replace('/', File.separatorChar)),
+				new File("src/test/resources/distribution/testAsnProd/cpu2/asn1vdm.h".replace('/', File.separatorChar)));
+
+		/** 2. Run cmake pr. CPU  **/
+
+		String cmake = "cmake";
+		cmake = "/usr/local/bin/cmake";
+		ProcessBuilder pb = new ProcessBuilder(cmake,".");
+		CMakeUtil cmakeUtil = new CMakeUtil(new File("ss"), new File("src/test/resources/CMakeLists.txt"), false);
+
+		// cpu1
+		pb.directory(cpu1Dir);
+		cmakeUtil.runProcess(pb, true);
+		// cpu2
+		pb.directory(cpu2Dir);
+		cmakeUtil.runProcess(pb, true);
+
+		/** 3. Run make pr. CPU **/
+
+		String make = "make";
+		ProcessBuilder pb2 = new ProcessBuilder(make);
+
+		// cpu1
+		pb2.directory(cpu1Dir);
+		cmakeUtil.runProcess(pb2, false);
+
+		// cpu2
+		pb2.directory(cpu2Dir);
+		cmakeUtil.runProcess(pb2, false);
+
+		/** 4. Run the exetutables **/
+
+		// cpu2 -- async call
+		String name = "cpu2Exe";
+		name = "./" + name;
+		ProcessBuilder pb3 = new ProcessBuilder(name);
+		pb3.directory(cpu2Dir);
+		pb3.start();
+
+		// cpu1 -- sync call
+		cmakeUtil.run(cpu1Dir, "cpu1Exe", TEST_OUTPUT != null);
+
+	}
 }
