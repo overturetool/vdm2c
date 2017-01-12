b6d08cf86594adbd3db8c69b7484ec849e45f1ec
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-01-12 15:41:42 +0100

    Fix issue with field look-ups
    Closes #59

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/FieldReadToFieldGetMacroTrans.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/FieldReadToFieldGetMacroTrans.java
@@ -1,11 +1,12 @@
 package org.overture.codegen.vdm2c.transformations;
 
 import static org.overture.codegen.vdm2c.utils.CTransUtil.GET_FIELD;
-import static org.overture.codegen.vdm2c.utils.CTransUtil.GET_FIELD_PTR_BYREF;
 import static org.overture.codegen.vdm2c.utils.CTransUtil.GET_FIELD_PTR;
+import static org.overture.codegen.vdm2c.utils.CTransUtil.GET_FIELD_PTR_BYREF;
 import static org.overture.codegen.vdm2c.utils.CTransUtil.createIdentifier;
 import static org.overture.codegen.vdm2c.utils.CTransUtil.newMacroApply;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 
@@ -24,12 +25,15 @@ import org.overture.ast.statements.AMapSeqStateDesignator;
 import org.overture.ast.types.AFunctionType;
 import org.overture.ast.types.AOperationType;
 import org.overture.cgc.extast.analysis.DepthFirstAnalysisCAdaptor;
+import org.overture.codegen.ir.STypeIR;
 import org.overture.codegen.ir.analysis.AnalysisException;
 import org.overture.codegen.ir.declarations.AFieldDeclIR;
 import org.overture.codegen.ir.declarations.SClassDeclIR;
 import org.overture.codegen.ir.expressions.AFieldExpIR;
 import org.overture.codegen.ir.expressions.AIdentifierVarExpIR;
 import org.overture.codegen.ir.statements.AAssignToExpStmIR;
+import org.overture.codegen.ir.types.AClassTypeIR;
+import org.overture.codegen.ir.types.AUnionTypeIR;
 import org.overture.codegen.trans.assistants.TransAssistantIR;
 import org.overture.codegen.vdm2c.extast.expressions.AMacroApplyExpIR;
 import org.overture.codegen.vdm2c.utils.CTransUtil;
@@ -97,16 +101,18 @@ public class FieldReadToFieldGetMacroTrans extends DepthFirstAnalysisCAdaptor
 				}
 			}
 		}
-
-		for(SClassDeclIR c : assist.getInfo().getClasses())
-		{
-			String fieldClass = fieldUtil.lookupFieldClass(c,  node.getMemberName());
-			if(fieldClass != null)
-			{
-				fieldClassName = fieldClass;
-			}
-		}
 		
+		AClassTypeIR classType = getClassTypeOfObject(node.getObject().getType());
+		
+		if (classType != null)
+		{
+			fieldClassName = classType.getName();
+		} else
+		{
+			logger.warn("Could not find class type of " + node.getObject());
+			return;
+		}
+			
 		AMacroApplyExpIR apply = newMacroApply(isThis ? GET_FIELD_PTR : GET_FIELD);
 		
 		assist.replaceNodeWith(node, apply);
@@ -122,6 +128,29 @@ public class FieldReadToFieldGetMacroTrans extends DepthFirstAnalysisCAdaptor
 		apply.setType(node.getType());
 	}
 	
+	private AClassTypeIR getClassTypeOfObject(STypeIR objType)
+	{
+		if(objType instanceof AClassTypeIR)
+		{
+			return (AClassTypeIR) objType;
+		}
+		if(objType instanceof AUnionTypeIR)
+		{
+			LinkedList<STypeIR> types = ((AUnionTypeIR) objType).getTypes();
+			
+			for(STypeIR t : types)
+			{
+				AClassTypeIR classType = getClassTypeOfObject(t);
+				
+				if(classType != null)
+				{
+					return classType;
+				}
+			}
+		}
+		
+		return null;
+	}
 
 	@Override
 	public void caseAIdentifierVarExpIR(AIdentifierVarExpIR node)
