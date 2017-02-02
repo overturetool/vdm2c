68b28ea555729bb71c9f6c3336b3fb65d9dff2cc
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-02-02 17:39:18 +0100

    Update test system to generate a fixture class

---
+++ core/vdm2c/src/test/resources/TestFlowFunctions.h
@@ -0,0 +1,25 @@
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+// Fixture template that does nothing by default
+class TestFlowFunctions : public testing::Test
+{
+public:
+  TestFlowFunctions()
+  {
+    
+  }
+  ~TestFlowFunctions()
+  {
+    
+  }
+  void SetUp()
+  {
+    
+  }
+  void TearDown(){
+    
+  }
+};
