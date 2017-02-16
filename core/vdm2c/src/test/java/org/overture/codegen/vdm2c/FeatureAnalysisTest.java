3d3fb06ba9e771ec531b97479462375b9d85123f
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-02-16 10:20:33 +0100

    Test feature analyses
    Addresses #72

---
+++ core/vdm2c/src/test/java/org/overture/codegen/vdm2c/FeatureAnalysisTest.java
@@ -0,0 +1,182 @@
+package org.overture.codegen.vdm2c;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.overture.ast.definitions.SClassDefinition;
+import org.overture.ast.lex.Dialect;
+import org.overture.codegen.vdm2c.analysis.FeatureAnalysisResult;
+import org.overture.config.Release;
+import org.overture.config.Settings;
+import org.overture.typechecker.util.TypeCheckerUtil;
+import org.overture.typechecker.util.TypeCheckerUtil.TypeCheckResult;
+
+public class FeatureAnalysisTest
+{
+	@BeforeClass
+	public static void setUp()
+	{
+		Settings.dialect = Dialect.VDM_RT;
+		Settings.release = Release.VDM_10;
+	}
+
+	/*
+	 * Sets
+	 */
+
+	@Test
+	public void usesSetEnum()
+	{
+		List<SClassDefinition> ast = buildAst("class A values s = {} end A");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertTrue(anRes.usesSets());
+		Assert.assertFalse(anRes.usesSeqs());
+		Assert.assertFalse(anRes.usesMaps());
+		assertNoLibs(anRes);
+	}
+
+	@Test
+	public void usesSet1Type()
+	{
+		List<SClassDefinition> ast = buildAst("class A instance variables xs : set1 of nat; end A");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertTrue(anRes.usesSets());
+		Assert.assertFalse(anRes.usesSeqs());
+		Assert.assertFalse(anRes.usesMaps());
+		assertNoLibs(anRes);
+	}
+
+	@Test
+	public void noSets()
+	{
+		List<SClassDefinition> ast = buildAst("class A end A");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertFalse(anRes.usesSets());
+		Assert.assertFalse(anRes.usesSeqs());
+		Assert.assertFalse(anRes.usesMaps());
+		assertNoLibs(anRes);
+	}
+
+	/*
+	 * Sequences
+	 */
+
+	@Test
+	public void usesSeqEnum()
+	{
+		List<SClassDefinition> ast = buildAst("class A values s = [] end A");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertFalse(anRes.usesSets());
+		Assert.assertTrue(anRes.usesSeqs());
+		Assert.assertFalse(anRes.usesMaps());
+		assertNoLibs(anRes);
+	}
+
+	@Test
+	public void usesSeq1Type()
+	{
+		List<SClassDefinition> ast = buildAst("class A instance variables xs : seq1 of nat; end A");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertFalse(anRes.usesSets());
+		Assert.assertTrue(anRes.usesSeqs());
+		Assert.assertFalse(anRes.usesMaps());
+		assertNoLibs(anRes);
+	}
+
+	@Test
+	public void noSeqs()
+	{
+		List<SClassDefinition> ast = buildAst("class A end A");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertFalse(anRes.usesSets());
+		Assert.assertFalse(anRes.usesSeqs());
+		Assert.assertFalse(anRes.usesMaps());
+		assertNoLibs(anRes);
+	}
+
+	/*
+	 * Maps
+	 */
+
+	@Test
+	public void usesMapEnum()
+	{
+		List<SClassDefinition> ast = buildAst("class A values s = {|->} end A");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertFalse(anRes.usesSets());
+		Assert.assertFalse(anRes.usesSeqs());
+		Assert.assertTrue(anRes.usesMaps());
+		assertNoLibs(anRes);
+	}
+
+	@Test
+	public void usesInjectiveMaps()
+	{
+		List<SClassDefinition> ast = buildAst("class A values xs : [inmap nat to nat] = nil; end A");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertFalse(anRes.usesSets());
+		Assert.assertFalse(anRes.usesSeqs());
+		Assert.assertTrue(anRes.usesMaps());
+		assertNoLibs(anRes);
+	}
+
+	@Test
+	public void noMaps()
+	{
+		List<SClassDefinition> ast = buildAst("class A end A");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertFalse(anRes.usesSets());
+		Assert.assertFalse(anRes.usesSeqs());
+		Assert.assertFalse(anRes.usesMaps());
+		assertNoLibs(anRes);
+	}
+
+	/*
+	 * Libraries
+	 */
+
+	@Test
+	public void usesLibs()
+	{
+		// CSV is a subclass of IO
+		List<SClassDefinition> ast = buildAst("class CSV end CSV"
+				+ " class MATH end MATH " + "class VDMUnit end VDMUnit "
+				+ "class VDMUtil end VDMUtil");
+		FeatureAnalysisResult anRes = FeatureAnalysisResult.runAnalysis(ast);
+		Assert.assertFalse(anRes.usesSets());
+		Assert.assertFalse(anRes.usesSeqs());
+		Assert.assertFalse(anRes.usesMaps());
+
+		Assert.assertTrue(anRes.usesCsvLib());
+		Assert.assertTrue(anRes.usesIoLib());
+		Assert.assertTrue(anRes.usesMathLib());
+		Assert.assertTrue(anRes.usesVdmUnit());
+		Assert.assertTrue(anRes.usesVdmUtil());
+	}
+
+	/*
+	 * Utility methods
+	 */
+
+	private void assertNoLibs(FeatureAnalysisResult anRes)
+	{
+		Assert.assertFalse(anRes.usesCsvLib());
+		Assert.assertFalse(anRes.usesIoLib());
+		Assert.assertFalse(anRes.usesMathLib());
+		Assert.assertFalse(anRes.usesVdmUnit());
+		Assert.assertFalse(anRes.usesVdmUtil());
+	}
+
+	private List<SClassDefinition> buildAst(String contents)
+	{
+		TypeCheckResult<List<SClassDefinition>> tcRes = TypeCheckerUtil.typeCheckPp(contents);
+
+		Assert.assertTrue("Could not type check model", tcRes != null
+				&& tcRes.parserResult.errors.isEmpty()
+				&& tcRes.errors.isEmpty());
+
+		return tcRes.result;
+	}
+}
