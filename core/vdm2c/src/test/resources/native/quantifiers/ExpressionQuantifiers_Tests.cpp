77af8646faf669047a1e3bcb2cfef206d4ff0afd
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-03-30 13:50:39 +0200

    More testing of 'forall' and 'exists'
    Addresses #82

--- core/vdm2c/src/test/resources/native/quantifiers/ExpressionQuantifiers_Tests.cpp
+++ core/vdm2c/src/test/resources/native/quantifiers/ExpressionQuantifiers_Tests.cpp
@@ -16,14 +16,24 @@ EXPECT_EQ (true,res->value.boolVal);\
 vdmFree(res);\
 vdmFree(c)
 
-TEST_F(TestFlowFunctions, forAllSimple)
+TEST_F(TestFlowFunctions, forAllFirst)
 {
-	CHECK(CLASS_ExpressionQuantifiers__Z12forAllSimpleEV);
+	CHECK(CLASS_ExpressionQuantifiers__Z11forAllFirstEV);
 }
 
-TEST_F(TestFlowFunctions, existsSimple)
+TEST_F(TestFlowFunctions, forAllSecond)
 {
-	CHECK(CLASS_ExpressionQuantifiers__Z12existsSimpleEV);
+	CHECK(CLASS_ExpressionQuantifiers__Z12forAllSecondEV);
 }
 
+
+TEST_F(TestFlowFunctions, existsFirst)
+{
+	CHECK(CLASS_ExpressionQuantifiers__Z11existsFirstEV);
+}
+
+TEST_F(TestFlowFunctions, existsSecond)
+{
+	CHECK(CLASS_ExpressionQuantifiers__Z12existsSecondEV);
+}
 
