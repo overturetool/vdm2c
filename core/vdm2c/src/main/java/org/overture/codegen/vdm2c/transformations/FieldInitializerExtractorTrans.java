52ae1b881a4fc6ec15e4607bb5e96c661e86b2c9
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-03-31 09:20:22 +0200

    Pass 'this' to field initialisers
    Closes #83

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/FieldInitializerExtractorTrans.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/FieldInitializerExtractorTrans.java
@@ -10,15 +10,26 @@ import org.overture.codegen.ir.SStmIR;
 import org.overture.codegen.ir.STypeIR;
 import org.overture.codegen.ir.analysis.AnalysisException;
 import org.overture.codegen.ir.declarations.AFieldDeclIR;
+import org.overture.codegen.ir.declarations.AFormalParamLocalParamIR;
 import org.overture.codegen.ir.declarations.AMethodDeclIR;
 import org.overture.codegen.ir.declarations.SClassDeclIR;
+import org.overture.codegen.ir.expressions.AApplyExpIR;
+import org.overture.codegen.ir.expressions.AIdentifierVarExpIR;
+import org.overture.codegen.ir.types.AExternalTypeIR;
 import org.overture.codegen.trans.assistants.TransAssistantIR;
 import org.overture.codegen.vdm2c.Vdm2cTag;
 import org.overture.codegen.vdm2c.Vdm2cTag.MethodTag;
 import org.overture.codegen.vdm2c.utils.NameMangler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FieldInitializerExtractorTrans extends DepthFirstAnalysisCAdaptor
 {
+	private static final String THIS_PARAM = "this";
+	private static final String THIS_ARG = "this_ptr";
+
+	final static Logger logger = LoggerFactory.getLogger(FieldInitializerExtractorTrans.class);
+	
 	private static final String FIELD_INITIALIZER = "fieldInitializer";
 	public TransAssistantIR assist;
 
@@ -43,8 +54,37 @@ public class FieldInitializerExtractorTrans extends DepthFirstAnalysisCAdaptor
 
 			AMethodDeclIR method = newInternalMethod(assist.getInfo().getTempVarNameGen().nextVarName(FIELD_INITIALIZER), body, type,true);
 			Vdm2cTag.addMethodTag(method, MethodTag.FieldInitializer);
+			
+			SClassDeclIR enclosingClass = node.getAncestor(SClassDeclIR.class);
+			
+			AExternalTypeIR extType = null;
+			
+			if (!node.getStatic() && !node.getFinal()) {
+				if (enclosingClass != null) {
+					
+					String paramTypeName = enclosingClass.getName() + "CLASS";
+					extType = new AExternalTypeIR();
+					extType.setName(paramTypeName);
 
-			node.setInitial(newApply(NameMangler.mangle(method)));
+					AFormalParamLocalParamIR param = new AFormalParamLocalParamIR();
+					param.setType(extType.clone());
+					param.setPattern(assist.getInfo().getPatternAssistant().consIdPattern(THIS_PARAM));
+
+					method.getFormalParams().add(param);
+				} else {
+					logger.error("Could not find enclosing class for field: " + node);
+				}
+			}
+
+			AApplyExpIR newApply = newApply(NameMangler.mangle(method));
+			
+			if(extType != null)
+			{
+				AIdentifierVarExpIR thisArg = assist.getInfo().getExpAssistant().consIdVar(THIS_ARG, extType);
+				newApply.getArgs().add(thisArg);
+			}
+			
+			node.setInitial(newApply);
 
 			SClassDeclIR cls = node.getAncestor(SClassDeclIR.class);
 			cls.getMethods().add(0, method);
