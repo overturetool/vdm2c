0fcdbb7a21100a86be108ef1f653b8fa3d3cac75
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-04-06 10:50:37 +0200

    Transform set enumerations to apply expressions
    Addresses #87

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/ColTrans.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/ColTrans.java
@@ -9,6 +9,7 @@ import org.overture.codegen.ir.SExpIR;
 import org.overture.codegen.ir.analysis.AnalysisException;
 import org.overture.codegen.ir.analysis.intf.IAnalysis;
 import org.overture.codegen.ir.expressions.AEnumSeqExpIR;
+import org.overture.codegen.ir.expressions.AEnumSetExpIR;
 import org.overture.codegen.ir.expressions.AExternalExpIR;
 import org.overture.codegen.ir.types.AExternalTypeIR;
 import org.overture.codegen.trans.assistants.TransAssistantIR;
@@ -16,56 +17,60 @@ import org.overture.codegen.vdm2c.utils.IApplyAssistant;
 
 /**
  *
- * TODO: Extend this class to handle sets and maps too (rename class accordingly)
+ * TODO: Extend this class to handle sets and maps too (rename class
+ * accordingly)
  * 
  */
-public class ColTrans extends DepthFirstAnalysisCAdaptor implements
-IApplyAssistant {
+public class ColTrans extends DepthFirstAnalysisCAdaptor implements IApplyAssistant {
 
 	public static final String SEQ_VAR = "newSeqVar";
-	
+	public static final String SET_VAR = "newSetVar";
+
 	private TransAssistantIR assist;
-	
-	public ColTrans(TransAssistantIR assist)
-	{
+
+	public ColTrans(TransAssistantIR assist) {
 		this.assist = assist;
 	}
 
 	@Override
-	public TransAssistantIR getAssist()
-	{
+	public TransAssistantIR getAssist() {
 		return assist;
 	}
 
 	@Override
-	public IAnalysis getAnalyzer()
-	{
+	public IAnalysis getAnalyzer() {
 		return THIS;
 	}
-	
+
 	@Override
 	public void caseAEnumSeqExpIR(AEnumSeqExpIR node) throws AnalysisException {
 
 		rewriteColEnumToApply(node, SEQ_VAR, node.getMembers());
 	}
 
-	private void rewriteColEnumToApply(AEnumSeqExpIR node, String seqVar, LinkedList<SExpIR> members) throws AnalysisException {
+	@Override
+	public void caseAEnumSetExpIR(AEnumSetExpIR node) throws AnalysisException {
+
+		rewriteColEnumToApply(node, SET_VAR, node.getMembers());
+	}
+
+	private void rewriteColEnumToApply(SExpIR node, String seqVar, LinkedList<SExpIR> members)
+			throws AnalysisException {
 		AExternalTypeIR extType = new AExternalTypeIR();
 		extType.setName("size_t");
-		
+
 		AExternalExpIR size = new AExternalExpIR();
 		size.setTargetLangExp(members.size() + "");
-		
+
 		LinkedList<SExpIR> argList = new LinkedList<>(members);
 		argList.addFirst(size);
-		
-		if(members.isEmpty())
-		{
+
+		if (members.isEmpty()) {
 			argList.add(assist.getInfo().getExpAssistant().consNullExp());
 		}
-		
+
 		SExpIR[] args = argList.toArray(new SExpIR[0]);
-		
+
 		rewriteToApply(this, node, seqVar, args);
 	}
 }
