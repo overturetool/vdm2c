dfa0abc7b067ef83da1577bfe4ca4f78bbb882ea
Victor Bandur <vpb500@york.ac.uk>
2017-04-21 13:13:56 +0100

    Reverted "Cleanup" commit causing failure on build server.

--- c/vdmclib/src/main/VdmGC.c
+++ c/vdmclib/src/main/VdmGC.c
@@ -166,8 +166,8 @@ void vdm_gc()
 		//This is the case when the value is created in-place or when freed using vdmFree().
 		if(current->loc->ref_from == NULL)
 		{
-			vdmFree_GCInternal(tmp_loc);
 			remove_allocd_mem_node(current);
+			vdmFree_GCInternal(tmp_loc);
 		}
 		else if(*(current->loc->ref_from) != current->loc)
 		{
