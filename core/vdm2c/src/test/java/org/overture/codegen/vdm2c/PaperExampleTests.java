6183022185a4e2990832a443277768b81d6e4ba5
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-05-03 09:55:38 +0200

    Add paper example tests

---
+++ core/vdm2c/src/test/java/org/overture/codegen/vdm2c/PaperExampleTests.java
@@ -0,0 +1,25 @@
+package org.overture.codegen.vdm2c;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+import org.overture.codegen.vdm2c.CMakeUtil.CMakeGenerateException;
+
+/**
+ * This test checks that the examples used in one of the VDM2C papers generate
+ * and compile correctly. All the examples used in this test are "external"
+ * models that are believed to be suitable candidates for analysing the
+ * performance of VDM2C (execution time and memory usage). This test does not
+ * yet execute any of the generated code.
+ * 
+ * All the models ship with Overture version 2.4.6.
+ * 
+ */
+public class PaperExampleTests extends MultiFileModelTestBase {
+
+	@Test
+	public void alarm() throws IOException, InterruptedException, CMakeGenerateException {
+		runTest(new File("src/test/resources/vdmrt/paper-examples/alarm"));
+	}
+}
