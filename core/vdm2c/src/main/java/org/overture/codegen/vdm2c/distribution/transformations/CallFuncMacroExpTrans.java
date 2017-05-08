e9f16308af37fb58eb7e5d649c579b9376cad57e
Miran <miranh89@gmail.com>
2017-05-08 17:16:25 +0200

    Clean up of distribution macro transformation

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/distribution/transformations/CallFuncMacroExpTrans.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/distribution/transformations/CallFuncMacroExpTrans.java
@@ -1,8 +1,6 @@
 package org.overture.codegen.vdm2c.distribution.transformations;
-import java.util.LinkedList;
 
 import org.overture.cgc.extast.analysis.DepthFirstAnalysisCAdaptor;
-import org.overture.codegen.ir.SExpIR;
 import org.overture.codegen.ir.analysis.AnalysisException;
 import org.overture.codegen.ir.expressions.AIdentifierVarExpIR;
 import org.overture.codegen.ir.expressions.AIntLiteralExpIR;
@@ -32,23 +30,17 @@ public class CallFuncMacroExpTrans extends DepthFirstAnalysisCAdaptor
 			AIdentifierVarExpIR id_exp = (AIdentifierVarExpIR) node.getRoot();
 			
 			if(id_exp.getName().equals("CALL_FUNC")){
-								
 				int args_len = node.getArgs().size() - 4;
+				AIntLiteralExpIR nrArgs = new AIntLiteralExpIR();
+				nrArgs.setValue((long) args_len);
 				
-				AIntLiteralExpIR a = new AIntLiteralExpIR();
-				a.setValue((long) args_len);
-				
-				// get super type name
+				// get super type name, and add to 3 position
 				AIdentifierVarExpIR supTy = (AIdentifierVarExpIR) node.getArgs().get(1).clone();
-				
 				supTy.setName("CLASS_ID_" + supTy.getName() + "_ID");
-				
-				//supTy.set
-				
 				node.getArgs().add(3, supTy);
 				
 				// Add number of function arguments to second last position
-				node.getArgs().add(4, a);
+				node.getArgs().add(4, nrArgs);
 
 				// Transform name to distribution macro
 				id_exp.setName("DIST_CALL");
