1478c4e17d5d4407aa17cb37e25e7de206486df8
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-05-18 15:02:51 +0200

    Check that all paper examples code-generate and compile #100

--- core/vdm2c/src/test/java/org/overture/codegen/vdm2c/PaperExampleTests.java
+++ core/vdm2c/src/test/java/org/overture/codegen/vdm2c/PaperExampleTests.java
@@ -1,6 +1,9 @@
 package org.overture.codegen.vdm2c;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -27,8 +30,34 @@ public class PaperExampleTests extends MultiFileModelTestBase {
 		Settings.release = Release.VDM_10;
 	}
 	
+	@Override
+	protected void assertTestsExecuted(File... tests) {
+		
+		// For now, we don't require any tests be executed.
+	}
+	
 	@Test
 	public void alarm() throws Exception {
 		runTest(new File("src/test/resources/vdmrt/paper-examples/alarm"));
+	}
+	
+	@Test
+	public void cashDispenser() throws Exception {
+		
+		List<File> extraCppFiles = Collections.singletonList(new File("src/test/resources/native/paper-examples/cash-dispenser-extension.c"));
+		
+		runTest(new File("src/test/resources/vdmrt/paper-examples/cash-dispenser"), extraCppFiles);
+	}
+	
+	@Test
+	public void pacemaker() throws Exception {
+		
+		List<File> extraCppFiles = Collections.singletonList(new File("src/test/resources/native/paper-examples/pacemaker-extension.c"));
+		runTest(new File("src/test/resources/vdmrt/paper-examples/pacemaker"), extraCppFiles);
+	}
+	
+	@Test
+	public void bubblesort() throws Exception {
+		runTest(new File("src/test/resources/vdmrt/paper-examples/bubblesort"));
 	}
 }
---
+++ core/vdm2c/src/test/resources/native/paper-examples/pacemaker-extension.c
@@ -0,0 +1,13 @@
+/*
+ * extension.c
+ *
+ *  Created on: May 18, 2017
+ *      Author: peter
+ */
+
+#include "Vdm.h"
+
+TVP vdm_Heart_FaultyHeart(TVP transaction)
+{
+  return 0;
+}
