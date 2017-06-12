2ff60560a75849b6f1a788acf4c1515f60cd20cb
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2017-06-12 16:53:01 +0200

    Add 'numFields' in the IR

--- core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/AddFieldTrans.java
+++ core/vdm2c/src/main/java/org/overture/codegen/vdm2c/transformations/AddFieldTrans.java
@@ -2,11 +2,13 @@ package org.overture.codegen.vdm2c.transformations;
 
 import org.overture.cgc.extast.analysis.DepthFirstAnalysisCAdaptor;
 import org.overture.codegen.ir.IRConstants;
+import org.overture.codegen.ir.SExpIR;
 import org.overture.codegen.ir.analysis.AnalysisException;
 import org.overture.codegen.ir.declarations.ADefaultClassDeclIR;
 import org.overture.codegen.ir.declarations.AFieldDeclIR;
 import org.overture.codegen.ir.expressions.AIntLiteralExpIR;
 import org.overture.codegen.ir.types.ANat1NumericBasicTypeIR;
+import org.overture.codegen.ir.types.ANatNumericBasicTypeIR;
 import org.overture.codegen.trans.assistants.TransAssistantIR;
 
 public class AddFieldTrans extends DepthFirstAnalysisCAdaptor
@@ -14,6 +16,8 @@ public class AddFieldTrans extends DepthFirstAnalysisCAdaptor
 	private TransAssistantIR assist;
 
 	private static final String NUM_FIELDS_PREFIX = "numFields_";
+	
+	private static final String NUM_FIELDS = "numFields";
 	
 	public AddFieldTrans(TransAssistantIR assist)
 	{
@@ -39,5 +43,31 @@ public class AddFieldTrans extends DepthFirstAnalysisCAdaptor
 			
 			node.getFields().addFirst(stateField);
 		}
+		
+		AFieldDeclIR field = new AFieldDeclIR();
+		field.setAccess(IRConstants.PRIVATE);
+		field.setType(new ANatNumericBasicTypeIR());
+		field.setStatic(false);
+		field.setFinal(false);
+		field.setVolatile(false);
+		field.setName(NUM_FIELDS);
+		field.setInitial(countInstanceFields(node));
+		
+		node.getFields().addFirst(field);
+	}
+	
+	private SExpIR countInstanceFields(ADefaultClassDeclIR node) {
+		
+		int noOfInstanceFields = 0;
+		
+		for(AFieldDeclIR f : node.getFields())
+		{
+			if(!f.getStatic() && !f.getFinal())
+			{
+				noOfInstanceFields++;
+			}
+		}
+		
+		return assist.getInfo().getExpAssistant().consIntLiteral(noOfInstanceFields);
 	}
 }
